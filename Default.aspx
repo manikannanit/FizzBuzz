1b1eac9fe3c219a299cf3312d85dd49316fb05b5
manikannanit <55607262+manikannanit@users.noreply.github.com>
2020-12-01 12:47:58 -0600

    Adding FizzBuzz app
    Adding FizzBuzz app

---
+++ Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="FizzBuzz._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="jumbotron">
+         <table border="0" >
+            <tr> <td >Enter the Numbers: </td>  <td><asp:TextBox ID="txtNumbers" runat="server" class="input-group-prepend" ></asp:TextBox></td> <td> <asp:Button ID="btnSubmit" runat="server" Text="Submit" class="btn btn-primary" OnClick="btnSubmit_Click"/></td></tr>
+              <tr> <td style="width: 149px"></td>   </tr>
+              <tr> <td style="width: 149px">Result:</td>  <td><asp:ListBox ID="txtResult1" runat="server" Height="240px" Width="144px"></asp:ListBox></td> </tr>
+            <tr> <td  colspan="2"><asp:Label ID="lblError" runat="server"></asp:Label></td>   </tr>
+        </table>
+
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+            
+        </div>
+        <div class="col-md-4">
+           
+        </div>
+        <div class="col-md-4">
+           
+        </div>
+    </div>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FizzBuzz
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string Numbers = txtNumbers.Text;
+            if (string.IsNullOrWhiteSpace(Numbers)) //validate the input control
+            {
+                lblError.Text = "The input is empty";
+            }
+            else
+            {
+                FizzBuzz(Numbers);
+            }
+        }
+        private void FizzBuzz(string FizzInput)
+        {
+            try
+            {
+                var Num = FizzInput.Split(',');
+
+                foreach (var i in Num)
+                    if (int.TryParse(i, out int n))
+                    {
+                        if (int.Parse(i) % 3 == 0 && int.Parse(i) % 5 == 0)
+                        {
+                            txtResult1.Items.Add("FizzBuzz");
+                        }
+                        else if (int.Parse(i) % 3 == 0)
+                        {
+                            txtResult1.Items.Add("Fizz");
+
+                        }
+                        else if (int.Parse(i) % 5 == 0)
+                        {
+                            txtResult1.Items.Add("Buzz");
+                        }
+                        else
+                        {
+                            txtResult1.Items.Add("Divided " + i + " By 3");
+                            txtResult1.Items.Add("Divided " + i + " By 5");
+                        }
+                    }
+                    else
+                        txtResult1.Items.Add("Invalid item");
+            }
+            catch (Exception e)
+            {
+                lblError.Text = e.Message;
+            }
+
+        }
+    }
+}
